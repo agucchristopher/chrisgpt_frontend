cedbafb39b7cc595abced3d8d9cdd4e6655326ea
Agu C. Christopher <aguchris740@gmail.com>
2023-02-10 15:11:55 -0800

    Added speech synthesis

--- script.js
+++ script.js
@@ -1,121 +1,123 @@
-import user from './assets/user.svg'
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 //let bot = 'https://img.icons8.com/ios/50/null/retro-robot.png'
-let bot = "https://img.icons8.com/ios/50/000000/retro-robot.png"
-let loadInterval
+let bot = "https://img.icons8.com/ios/50/000000/retro-robot.png";
+let loadInterval;
 
 function loader(element) {
-    element.textContent = ''
+  element.textContent = "";
 
-    loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += ".";
 
-        // If the loading indicator has reached three dots, reset it
-        if (element.textContent === '....') {
-            element.textContent = '';
-        }
-    }, 300);
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
 }
 
 function typeText(element, text) {
-    let index = 0
+  let index = 0;
 
-    let interval = setInterval(() => {
-        if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
-        } else {
-            clearInterval(interval)
-        }
-    }, 20)
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
 }
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
 function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-    return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-    return (
-        `
-        <div class="wrapper ${isAi && 'ai'}">
+  return `
+        <div class="wrapper ${isAi && "ai"}">
             <div class="chat">
                 <div class="profile">
                      <i class="fa-solid fa-message-bot"></i>
 <img 
-                      src=${isAi? bot : user} 
-                      alt="${'user'}" 
+                      src=${isAi ? bot : user} 
+                      alt="${"user"}" 
                     />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `
-    )
+    `;
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const data = new FormData(form)
+  const data = new FormData(form);
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-    // to clear the textarea input 
-    form.reset()
+  // to clear the textarea input
+  form.reset();
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  // specific message div
+  const messageDiv = document.getElementById(uniqueId);
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
 
-    const response = await fetch('https://openai-nodejs-api.cyclic.app/ai/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+  const response = await fetch("https://openai-nodejs-api.cyclic.app/ai/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = " ";
 
-    if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.reply.trim() // trims any trailing spaces/'\n' 
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.reply.trim(); // trims any trailing spaces/'\n'
+    let utterance = new SpeechSynthesisUtterance(data.reply);
+    speechSynthesis.cancel();
+    utterance.volume = 200;
+    utterance.rate = 1.2;
+    speechSynthesis.speak(utterance);
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
 
-        typeText(messageDiv, parsedData)
-    } else {
-        const err = await response.text()
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
-}
-
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
